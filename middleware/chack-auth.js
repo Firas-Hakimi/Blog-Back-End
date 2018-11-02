e6a07587fc722bc9e75214dd4b89cb9416863e52
Firas Hakimi <firas.hakimi@esprit.tn>
2018-11-02 12:13:45 +0100

    fix split method

--- middleware/chack-auth.js
+++ middleware/chack-auth.js
@@ -2,8 +2,8 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
     try{
-        console.log(req.headers.authorization);
-        const token = req.headers.authorization.sprit(" ")[1];
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
         jwt.verify(token, 'this_should_be_longer');
         next();
     } catch (error) {  
